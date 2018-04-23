4385e86cd090d14c106ef74af0819ecdad96c4b5
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 13:38:52 +0300

    new example system to dump entities to console so we can test stuff

---
+++ Reflective ECS/ReflectiveECS/Examples/ECS/DebugEntitiesSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReflectiveECS.Examples.ECS
+{
+    public class DebugEntitiesSystem : ISystem
+    {
+        [Execute(getEntityItself: true)]
+        public void Execute(Entity entity)
+        {
+            Console.WriteLine($"entity {entity.ID}");
+            foreach (var component in entity.Components)
+            {
+                Console.WriteLine("\t" + component.GetType().Name);
+
+                var fields = component.GetType().GetFields();
+                foreach (var fieldInfo in fields)
+                {
+                    Console.WriteLine($"\t\t{fieldInfo.Name} = {fieldInfo.GetValue(component)}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
