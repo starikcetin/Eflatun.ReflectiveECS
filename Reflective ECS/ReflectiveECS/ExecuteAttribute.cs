610159eda0982e603364964dce4940f1333cad2a
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 13:35:06 +0300

    Execute attribute created

---
+++ Reflective ECS/ReflectiveECS/ExecuteAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReflectiveECS
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ExecuteAttribute : Attribute
+    {
+        public bool GetEntityItself;
+
+        public ExecuteAttribute(bool getEntityItself = false)
+        {
+            GetEntityItself = getEntityItself;
+        }
+    }
+}
