72adabd8369fcf2560b01168949e27e1a9a8f8fb
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 10:49:30 +0300

    SystemsRunner class created.

---
+++ Reflective ECS/ReflectiveECS/SystemsRunner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReflectiveECS
+{
+    public class SystemsRunner
+    {
+        private readonly SystemsManager _systemsManager;
+
+        public SystemsRunner(SystemsManager systemsManager)
+        {
+            _systemsManager = systemsManager;
+        }
+
+        public void RunAll()
+        {
+            foreach (var system in _systemsManager.RegisteredSystems)
+            {
+                RunSystem(system);
+            }
+        }
+
+        private void RunSystem(ISystem system)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
