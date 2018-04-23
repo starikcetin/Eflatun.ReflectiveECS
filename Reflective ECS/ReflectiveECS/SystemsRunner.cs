b4eca78d200035665305b4a860d4d0933e4346a6
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 13:42:13 +0300

    SystemsRunner does the fucking job now!
    This is awesome, I feel like a god now.

--- Reflective ECS/ReflectiveECS/SystemsRunner.cs
+++ Reflective ECS/ReflectiveECS/SystemsRunner.cs
@@ -25,7 +25,42 @@ namespace ReflectiveECS
 
         private void Run(ISystem system)
         {
-            throw new NotImplementedException();
+            // get the execute method which is marked with [Execute] attribute
+            var concrete = system.GetType();
+            var executeMethod = concrete.GetMethods()
+                .Single(m => m.GetCustomAttributes(typeof(ExecuteAttribute), false).Length > 0);
+
+            // will the entity itself be passed?
+            var executeAttribute = (ExecuteAttribute) executeMethod.GetCustomAttribute(typeof(ExecuteAttribute));
+            var getEntityItself = executeAttribute.GetEntityItself;
+
+            // get all parameters as components
+            var parameters = executeMethod.GetParameters();
+            var parameterTypes =
+                (getEntityItself ? parameters.Skip(1) : parameters) // if the entity itself will be passed, skip the first parameter
+                .Select(pi => pi.ParameterType).ToArray();
+
+            // match parameter components
+            var matchedEntities = _entitiesManager.GetMatchAll(parameterTypes);
+
+            // execute for all matches
+            foreach (var entity in matchedEntities)
+            {
+                // construct parameter array
+                var parameterList = new List<object>();
+
+                // if the entity itself will be passed, add it to the parameter array now
+                if (getEntityItself) parameterList.Add(entity);
+
+                foreach (var parameterType in parameterTypes)
+                {
+                    var componentInstance = entity.Get(parameterType);
+                    parameterList.Add(componentInstance);
+                }
+
+                // invoke execute method of system
+                executeMethod.Invoke(system, parameterList.ToArray());
+            }
         }
     }
 }
