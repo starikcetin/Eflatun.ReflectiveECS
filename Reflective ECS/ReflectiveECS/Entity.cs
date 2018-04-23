e98f7fc00ca86cf196b46e47547687ddc56c5a93
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 10:47:41 +0300

    Entity class created

---
+++ Reflective ECS/ReflectiveECS/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ReflectiveECS
+{
+    public class Entity
+    {
+        private List<IComponent> _components;
+
+        public void Register(IComponent component)
+        {
+            _components.Add(component);
+        }
+
+        public void Deregister(IComponent component)
+        {
+            _components.Remove(component);
+        }
+    }
+}
