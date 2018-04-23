c200301cde98a304dcc8502b372ee65320b3404e
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 10:50:00 +0300

    Program.cs edited with typical template intiailization.

--- Reflective ECS/ReflectiveECS/Program.cs
+++ Reflective ECS/ReflectiveECS/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace ReflectiveECS
+﻿using ReflectiveECS.Examples.ECS;
+
+namespace ReflectiveECS
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var systemsManager = new SystemsManager();
+            var entitiesManager = new EntitiesManager();
+            var systemsRunner = new SystemsRunner(systemsManager);
+
+
+            //
+            // Systems
+            //
+
+            var posAndRotSystem = new PosAndRotSys();
+            var rotSystem = new RotSys();
+
+            systemsManager.Register(posAndRotSystem);
+            systemsManager.Register(rotSystem);
+
+
+            //
+            // Entities
+            //
+
+            var posAndRotEntity = new Entity();
+            posAndRotEntity.Register(new PosComp());
+            posAndRotEntity.Register(new RotComp());
+
+            var rotEntity = new Entity();
+            rotEntity.Register(new RotComp());
+
+            entitiesManager.Register(posAndRotEntity);
+            entitiesManager.Register(rotEntity);
+
+
+            //
+            // Execution
+            //
+
+            while (true)
+            {
+                systemsRunner.RunAll();
+            }
         }
     }
 }
