90dddf2882f28a154c626c5defd4fd9c7f822e3a
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 10:48:10 +0300

    SystemsManager class created.

---
+++ Reflective ECS/ReflectiveECS/SystemsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ReflectiveECS
+{
+    public class SystemsManager
+    {
+        private readonly List<ISystem> _registeredSystems;
+        public ReadOnlyCollection<ISystem> RegisteredSystems => _registeredSystems.AsReadOnly();
+
+        public SystemsManager()
+        {
+            _registeredSystems = new List<ISystem>();
+        }
+
+        public void Register(ISystem system)
+        {
+            _registeredSystems.Add(system);
+        }
+
+        public void Unregister(ISystem system)
+        {
+            _registeredSystems.Remove(system);
+        }
+    }
+}
