126121cef780c46b9fb61c8900099bd2b5625eca
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-23 10:47:57 +0300

    EntititesManager class created

---
+++ Reflective ECS/ReflectiveECS/EntitiesManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ReflectiveECS
+{
+    public class EntitiesManager
+    {
+        private List<Entity> _entities;
+
+        public void Register(Entity entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void Unregister(Entity entity)
+        {
+            _entities.Remove(entity);
+        }
+    }
+}
