f85d1c3428067c2858989c9d4255e72b7461ad4f
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-24 19:12:44 +0300

    Better performance testing environment
    1. Seperate class for initializing and running the tests.
    2. Clean main.

--- Reflective ECS/Development/Program.cs
+++ Reflective ECS/Development/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Development.ECS;
-using ReflectiveECS.Core.ECS;
-using ReflectiveECS.Core.Managers;
+﻿using Development.Testing;
 
 namespace Development
 {
@@ -11,119 +6,16 @@ namespace Development
     {
         public static void Main(string[] args)
         {
-            var systemsDatabase = new SystemsDatabase();
-            var entitiesDatabase = new EntitiesDatabase();
-            var systemsRunner = new SystemsRunner(systemsDatabase, entitiesDatabase);
-            var entityIdManager = new EntityIdManager();
+            var performanceTesting = new PerformanceTester(
+                frameCount: 50,
+                systemCount: 50,
+                entityCount: 10_000,
+                componentPerEntity: 10,
+                clearConsoleBeforeFinal: true,
+                beepOnEnd: true
+                );
 
-
-            //
-            // Systems
-            //
-            var pars0 = new PosAndRotSys();
-            var pars1 = new PosAndRotSys();
-            var pars2 = new PosAndRotSys();
-            var pars3 = new PosAndRotSys();
-            var pars4 = new PosAndRotSys();
-
-            var rs0 = new RotSys();
-            var rs1 = new RotSys();
-            var rs2 = new RotSys();
-            var rs3 = new RotSys();
-            var rs4 = new RotSys();
-
-            systemsDatabase.Register(pars0);
-            systemsRunner.Cache(pars0);
-
-            systemsDatabase.Register(pars1);
-            systemsRunner.Cache(pars1);
-
-            systemsDatabase.Register(pars2);
-            systemsRunner.Cache(pars2);
-
-            systemsDatabase.Register(pars3);
-            systemsRunner.Cache(pars3);
-
-            systemsDatabase.Register(pars4);
-            systemsRunner.Cache(pars4);
-
-            systemsDatabase.Register(rs0);
-            systemsRunner.Cache(rs0);
-
-            systemsDatabase.Register(rs1);
-            systemsRunner.Cache(rs1);
-
-            systemsDatabase.Register(rs2);
-            systemsRunner.Cache(rs2);
-
-            systemsDatabase.Register(rs3);
-            systemsRunner.Cache(rs3);
-
-            systemsDatabase.Register(rs4);
-            systemsRunner.Cache(rs4);
-
-            //var des = new DebugEntitiesSystem();
-            //systemsDatabase.Register(des);
-            //systemsRunner.Cache(des);
-
-
-            //
-            // Entities
-            //
-
-            var posAndRotEntity = new Entity(entityIdManager.GetUniqueId());
-            posAndRotEntity.Register(new PosComp { X = 10, Y = 20 });
-            posAndRotEntity.Register(new RotComp { Angle = 100 });
-
-            var rotEntity = new Entity(entityIdManager.GetUniqueId());
-            rotEntity.Register(new RotComp { Angle = 200 });
-
-            entitiesDatabase.Register(posAndRotEntity);
-            entitiesDatabase.Register(rotEntity);
-
-
-            //
-            // Execution
-            //
-
-            PerformanceTest(systemsRunner);
-        }
-
-        private static void PerformanceTest(SystemsRunner systemsRunner)
-        {
-            const int batchCount = 50;
-            const int batchSize = 500;
-
-            var elapses = new List<long>();
-            var sw = new Stopwatch();
-
-            for (var j = 0; j < batchCount; j++)
-            {
-                sw.Start();
-                for (var i = 0; i < batchSize; i++)
-                {
-                    systemsRunner.RunAll();
-                }
-
-                sw.Stop();
-                elapses.Add(sw.ElapsedMilliseconds);
-                sw.Reset();
-            }
-
-            Console.Clear();
-            Console.Beep();
-
-            long total = 0;
-            for (var i = 0; i < elapses.Count; i++)
-            {
-                var e = elapses[i];
-                Console.WriteLine($"Batch: {i} | Elapsed: {e} ms | Average: {e / (double) batchSize} ms");
-                total += e;
-            }
-
-            Console.WriteLine($"Total Elapsed: {total} ms |" +
-                              $" Batch Total Average: {total / (double) batchCount} ms |" +
-                              $" Overall Average: {total / (double) (batchCount * batchSize)} ms");
+            performanceTesting.Run();
         }
     }
 }
