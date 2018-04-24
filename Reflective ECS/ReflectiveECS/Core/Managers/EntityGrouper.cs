50a349aff4658f919a64d1d60a1a6a13382fcc3c
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-24 23:45:06 +0300

    EntityGrouper created

---
+++ Reflective ECS/ReflectiveECS/Core/Managers/EntityGrouper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using ReflectiveECS.Core.ECS;
+
+namespace ReflectiveECS.Core.Managers
+{
+    public class EntityGrouper
+    {
+        private readonly Dictionary<Type, HashSet<Entity>> _entityGroups = new Dictionary<Type, HashSet<Entity>>();
+
+        public void Register(Entity e, Type cType)
+        {
+            AddSafe(e, cType);
+        }
+
+        public void Unregister(Entity e, Type cType)
+        {
+            RemoveSafe(e, cType);
+        }
+
+        public HashSet<Entity> GetGroup(Type cType)
+        {
+            return _entityGroups[cType];
+        }
+
+        public HashSet<Entity> GetGroupIntersect(params Type[] cTypes)
+        {
+            HashSet<Entity> result = _entityGroups[cTypes[0]];
+
+            for (var i = 1; i < cTypes.Length; i++)
+            {
+                EnsureKeyPresent(cTypes[i]);
+                result.IntersectWith(_entityGroups[cTypes[i]]);
+            }
+
+            return result;
+        }
+
+        private void RemoveSafe(Entity entity, Type cType)
+        {
+            if (EnsureKeyPresent(cType))
+            {
+                _entityGroups[cType].Remove(entity);
+            }
+        }
+
+        private void AddSafe(Entity e, Type cType)
+        {
+            EnsureKeyPresent(cType);
+            _entityGroups[cType].Add(e);
+        }
+
+        // returns if the key was already present
+        private bool EnsureKeyPresent(Type cType)
+        {
+            if (!_entityGroups.ContainsKey(cType))
+            {
+                _entityGroups[cType] = new HashSet<Entity>();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
